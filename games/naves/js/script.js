7689ab858d4b8e9352fbc84bf4d486b5f1410a9d
ElderSam <eldersamuel98@gmail.com>
2020-12-23 22:32:55 -0300

    jogo de naves: movimentando jogador

--- games/naves/js/script.js
+++ games/naves/js/script.js
@@ -10,18 +10,67 @@ function start() {
     
     //Principais variáveis do jogo
     var jogo = {}
-        
+    var TECLA = {
+        W: 87,
+        UP: 38,
+
+        S: 83,
+        DOWN: 40,
+
+        D: 68,
+        RIGHT: 39,
+    }
+    
+    jogo.pressionou = [];
+
+	//Verifica se o usuário pressionou alguma tecla	
+	$(document).keydown(function(e){
+	    jogo.pressionou[e.which] = true;
+	});
+
+	$(document).keyup(function(e){
+        jogo.pressionou[e.which] = false;
+	});
+
     //Game Loop
     jogo.timer = setInterval(loop,30); //chama loop() a cada 30ms
 
     function loop() {
         movefundo();
+        moveJogador();
     }
 
     //Função que movimenta o fundo do jogo
 	function movefundo() {
         esquerda = parseInt($("#fundoGame").css("background-position"));
         $("#fundoGame").css("background-position",esquerda-1);   
+    }
+
+    function moveJogador() {
+
+        if (jogo.pressionou[TECLA.W] || jogo.pressionou[TECLA.UP]) {
+            var topo = parseInt($("#jogador").css("top")); //pega a altura do jogador
+            $("#jogador").css("top",topo-10); //desce
+
+            if(topo <= 0) { //limita até onde pode descer
+                $("#jogador").css("top", topo+10);
+            }
+        }
+        
+        if (jogo.pressionou[TECLA.S] || jogo.pressionou[TECLA.DOWN]) {   
+            var topo = parseInt($("#jogador").css("top")); //pega a altura do jogador
+            $("#jogador").css("top",topo+10); //sobe
+
+            if(topo >= 434) { //limita até onde pode subir
+                $("#jogador").css("top", topo-10);
+            }
+        }
+        
+        if (jogo.pressionou[TECLA.D] || jogo.pressionou[TECLA.RIGHT]) {
+            
+            //Chama função Disparo	
+        }
+    
     }
     
 }
